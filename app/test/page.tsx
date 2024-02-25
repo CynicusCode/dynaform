904e112cb70abd27d0ff0c19aa04a746ca894fa7
CynicusCode <114907376+CynicusCode@users.noreply.github.com>
2024-02-24 23:15:59 -0600

    finishing styling the page

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,29 +3,27 @@ import Image from "next/image";
 
 const TestPage = () => {
 	return (
-		<main className="m-0 flex h-screen items-center justify-center bg-stone-100 relative">
-			{/* Black background container */}
-			<div className="bg-black flex items-center justify-center h-screen max-w-7xl max-w-screen-2xl w-full">
-				<section
-					className="min-h-[85%] md:min-h-[80%] w-[90%] md:w-[60%] lg:max-w-2xl bg-white shadow-xl m-0 z-10 relative flex flex-col items-center justify-start pt-20 md:pt-10 md:ml-10 overflow-y-auto rounded-b-lg"
-					style={{ direction: "rtl" }}
-				>
-					<div
-						className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-[50%] overflow-hidden rounded-sm shadow-sm w-full max-w-[180px] sm:max-w-[230px] md:max-w-[300px] lg:max-w-[300px] xl:max-w-[300px]"
-						style={{ aspectRatio: "2 / 1" }}
-					>
-						<Image
-							src="/logo/banner.png"
-							alt="Banner"
-							width={200}
-							height={100}
-							layout="responsive"
-						/>
-					</div>
-					content goes here
+		<main className="bg-white relative flex justify-center items-center h-screen m-0">
+			<div className="relative max-w-screen-2xl w-full h-full flex justify-center items-center bg-stone-50">
+				{/* Adjusted container for responsive positioning and doubling size of the Image component at SM breakpoint */}
+				<div className="absolute z-50 rounded-xl overflow-hidden top-10 left-1/2 transform -translate-x-1/2 sm:w-[200px] sm:h-[100px] md:mr-[27%] md:left-auto md:transform-none lg:top-4 lg:w-[200px] lg:h-[100px]">
+					<Image
+						src="/logo/banner.png"
+						alt="Banner"
+						width={200}
+						height={100}
+						layout="responsive"
+						objectFit="cover"
+					/>
+				</div>
+				{/* Responsive margin applied to the section */}
+				<section className="h-[90%] w-[95%] bg-white shadow-2xl m-0 relative flex flex-col items-center justify-center pt-20 md:m-6 rounded-xl max-w-xl ">
+					Content goes here
 				</section>
-				<section className="hidden sm:flex md:w-[35%] md:max-w-md lg:max-w-lg h-[73%] md:-translate-x-10 bg-blue-900 shadow-sm m-0 z-10 relative justify-center items-center rounded-lg">
-					content goes here2
+
+				{/* Adjusted positioning for the blue section */}
+				<section className="hidden sm:flex h-[85%] md:-translate-x-14 bg-blue-900 w-[35%] -m-6 z-10 relative justify-center items-center rounded-xl max-w-md shadow-xl">
+					More content goes here
 				</section>
 			</div>
 		</main>
